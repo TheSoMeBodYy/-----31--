d362304112c347fc4a1827ea49004a8141e856ad
TheSoMeBodYy <91886673+TheSoMeBodYy@users.noreply.github.com>
2022-11-21 19:45:20 +0200

    Add files via upload

---
+++ ПІС-КР-КС31-Свиридова/проєкт/ProxyPatternDriver.java
@@ -0,0 +1,7 @@
+public class ProxyPatternDriver {
+    public static void main(String[] args) {
+        ExpensiveObject object = new ExpensiveObjectProxy();
+        object.process();
+        object.process();
+    }
+}
